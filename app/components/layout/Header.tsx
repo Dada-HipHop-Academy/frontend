d6433531e0392a02955a8f93e1e307c727c6ab05
Taher726 <chaltouttaher@gmail.com>
2025-12-19 17:50:43 +0100

    Highlight navItem of current page in the header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,20 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+    const pathName = usePathname();
+
+    const navItems = [
+        {href: "/", label: "Accueil"},
+        {href: "/a-propos", label: "À propos"},
+        {href: "/cours-activités", label: "Cours & Activités"},
+        {href: "/studio-musique", label: "Studio Musique"},
+        {href: "/workshops-evénements", label: "Workshops & Événements"},
+        {href: "/actualités", label: "Actualités"},
+        {href: "/dada-réseaux-artist", label: "Dada Réseaux Artist"}
+    ];
+
     return (
         <header className="fixed top-0 right-0 left-0 z-50 bg-white shadow-md py-2">
             <div className="container mx-auto px-4 flex justify-between items-center">
@@ -12,27 +26,11 @@ const Header = () => {
                 {/* Desktop Navigation */}
                 <nav className="hidden md:flex items-center space-x-7">
                     <div className="flex items-center space-x-7">
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Accueil
-                        </Link>
-                        <Link href="/a-propos" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            À propos
-                        </Link>
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Cours & Activités
-                        </Link>
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Studio Musique
-                        </Link>
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Workshops & Événements
-                        </Link>
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Actualités
-                        </Link>
-                        <Link href="/" className="bg-white hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md">
-                            Dada Réseaux Artist
-                        </Link>
+                        {navItems.map((item) => (
+                            <Link href={item.href} key={item.href} className={`hover:bg-[#4ECDC4] flex items-center cursor-pointer text-black py-1 px-2 rounded-md ${pathName === item.href ? "bg-[#4ECDC4]" : "bg-white"}`}>
+                                {item.label}
+                            </Link>
+                        ))}
                     </div>
                 </nav>
             </div>
